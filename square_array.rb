cd3ea086ce94182e11b44f027ed020dc8793629a
Christopher Volpe <christopher.volpe12@gmail.com>
2018-12-13 20:25:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,3 +6,11 @@ def square_array(array)
   array.each {|i| squared << i ** 2}
   squared
 end
+
+def square_array_2(array_2)
+  numbers = [1, 2, 3]
+  squared = []
+  new_numbers = [9, 10, 16, 25]
+  array_2.collect {|i| squared << i * i}
+  squared
+end
